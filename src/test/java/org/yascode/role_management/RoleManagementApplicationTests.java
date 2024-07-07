9d1c00536badd3ff81f14425858752036f463f79
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-07-07 20:43:24 +0100

    first commit

---
+++ src/test/java/org/yascode/role_management/RoleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yascode.role_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
