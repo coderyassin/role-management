3e3d68bcf3a22a3af34bff77db089209f516609d
073176 <yassin.mellouki@intelcia.com>
2024-07-18 15:10:24 +0100

    Feat: add engine

--- src/main/java/org/yascode/role_management/RoleManagementApplication.java
+++ src/main/java/org/yascode/role_management/RoleManagementApplication.java
@@ -1,34 +1,11 @@
 package org.yascode.role_management;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.yascode.role_management.service.UserRoleService;
 
 @SpringBootApplication
-public class RoleManagementApplication implements CommandLineRunner {
-	private final UserRoleService userRoleService;
-
-    public RoleManagementApplication(UserRoleService userRoleService) {
-        this.userRoleService = userRoleService;
-    }
-
+public class RoleManagementApplication {
     public static void main(String[] args) {
 		SpringApplication.run(RoleManagementApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		/*User user = User.builder()
-				.id(1L)
-				.username("yascode")
-				.age(38)
-				.seniority(4)
-				.profile("MANAGER_C3")
-				.department("IT")
-				.build();
-
-		userRoleService.assignRolesToUser(user)
-					   .forEach(System.out::println);*/
 	}
 }
--- src/main/java/org/yascode/role_management/service/UserRoleService.java
+++
@@ -1,9 +0,0 @@
-package org.yascode.role_management.service;
-
-import org.yascode.role_management.entity.User;
-
-import java.util.List;
-
-public interface UserRoleService {
-    List<String> assignRolesToUser(User user);
-}
